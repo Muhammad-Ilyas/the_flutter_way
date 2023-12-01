f7a2f7e0cdcca34e736b5fbdc366f25ad897eed2
Muhammad Ilyas <muhammad_ilyas192@hotmail.com>
2023-12-01 20:18:11 +0500

    added(product list): create array of products and display a listview

--- lib/ProductList.dart
+++ lib/ProductList.dart
@@ -9,6 +9,41 @@ class ProductList extends StatefulWidget {
 }
 
 class _MyWidgetState extends State<ProductList> {
+  List<String> productName = [
+    'Mango',
+    'Orange',
+    'Banana',
+    'Mango',
+    'Orange',
+    'Banana',
+    'Mango',
+    'Orange',
+    'Banana',
+  ];
+  List<String> productUnit = [
+    'KG',
+    'KG',
+    'DZN',
+    'KG',
+    'KG',
+    'DZN',
+    'KG',
+    'KG',
+    'DZN'
+  ];
+  List<int> productPrice = [200, 150, 100, 200, 150, 100];
+  List<String> productImage = [
+    'https://image.shutterstock.com/image-photo/mango-isolated-on-white-background-600w-610892249.jpg',
+    'https://image.shutterstock.com/image-photo/orange-sliced-green-leaves-isolated-600w-1386912362.jpg',
+    'https://image.shutterstock.com/image-photo/green-grape-leaves-isolated-on-600w-533487490.jpg',
+    'https://image.shutterstock.com/image-photo/mango-isolated-on-white-background-600w-610892249.jpg',
+    'https://image.shutterstock.com/image-photo/orange-sliced-green-leaves-isolated-600w-1386912362.jpg',
+    'https://image.shutterstock.com/image-photo/green-grape-leaves-isolated-on-600w-533487490.jpg',
+    'https://image.shutterstock.com/image-photo/mango-isolated-on-white-background-600w-610892249.jpg',
+    'https://image.shutterstock.com/image-photo/orange-sliced-green-leaves-isolated-600w-1386912362.jpg',
+    'https://image.shutterstock.com/image-photo/green-grape-leaves-isolated-on-600w-533487490.jpg',
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,6 +70,47 @@ class _MyWidgetState extends State<ProductList> {
           SizedBox(
             width: 50,
           )
+        ],
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+                itemCount: productName.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            mainAxisSize: MainAxisSize.max,
+                            children: [
+                              Image(
+                                  height: 100,
+                                  width: 100,
+                                  image: NetworkImage(
+                                    productImage[index].toString(),
+                                  )),
+                              Text(
+                                productName[index].toString(),
+                                style: const TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  );
+                }),
+          ),
         ],
       ),
     );
